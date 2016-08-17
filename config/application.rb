1eee1d7905fb271fdec6fa01fe96df06fce500bf
tristan-secord <tristan.secord@hotmail.com>
2016-08-17 01:00:37 -0400

    Debugging/Test Commit

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Skiy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+    config.autoload_paths += %W(#{config.root}/lib)
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
