4319d9f89c4927b0c80a48ec855ed6b462b64025
tristan-secord <tristan.secord@hotmail.com>
2016-09-20 22:46:29 -0400

    Debugging/Test Commit

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -324,6 +324,7 @@ class ApiController < ApplicationController
 							@notification.save
 							#get pending notifications count
 							@friend_notifications = PendingNotification.where('user_id = ? AND read = ? AND (expiry IS NULL OR expiry > ?)', params[:id], false, Time.now)
+							@friend_device = Device.where(:user_id => params[:id]).first
 							if @friend_device && @friend_device.authtoken_expiry > Time.now && @friend_device.registration_id
 								User.notify_ios(params[:id], "REQUEST_LOCATION", @payload, @friend_notifications.count, @session.as_json)
 							end
