d98de21bc354e1831fc174376055bd7e7d97624a
tristan-secord <tristan.secord@hotmail.com>
2016-09-21 13:30:42 -0400

    Debugging/Test Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,7 +62,7 @@ class User < ActiveRecord::Base
 		# take a look at the docs about these params
 		notification.badge = badgeCount
 		notification.sound = "sosumi.aiff"
-		notification.custom_data = data
+		notification.custom_data = {foo: "bar"}
 		apn.push(notification)
 	end
 
