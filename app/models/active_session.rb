576a6cbd8def7c0ab189dedc9ec6db28fa802daa
tristan-secord <tristan.secord@hotmail.com>
2016-09-20 22:28:35 -0400

    Debugging/Test Commit

--- app/models/active_session.rb
+++ app/models/active_session.rb
@@ -5,5 +5,5 @@ class ActiveSession < ActiveRecord::Base
 	validates_presence_of :friend_id
 	validates_presence_of :status
 	validates_presence_of :channel_name
-	validates_presence_of :type
+	validates_presence_of :request_type
 end
