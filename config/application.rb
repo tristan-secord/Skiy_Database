e8e7b1c5f7c59b56074dc4a667ece09620694cbc
tristan-secord <tristan.secord@hotmail.com>
2016-09-22 15:34:57 -0400

    Debugging/Test Commit

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Skiy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-     config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+    config.public_file_server.enabled = true
     config.assets.initialize_on_precompile = false
     config.log_level = :debug
   end
