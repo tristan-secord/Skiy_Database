c9459b8491f86c9ff1d7e624161d6dc2f706ba14
tristan-secord <tristan.secord@hotmail.com>
2016-09-22 21:59:23 -0400

    Debugging/Test Commit

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-  	stream_from "roomChannel"
+  	stream_from "room_channel"
     # stream_from "some_channel"
   end
 
