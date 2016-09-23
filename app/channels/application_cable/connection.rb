c44b1c72d341cce5df4b924200111804f9f08c62
tristan-secord <tristan.secord@hotmail.com>
2016-09-23 12:57:36 -0400

    Debugging/Test Commit

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,20 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	identified_by :current_user
+
+  	include ActionController::HttpAuthentication::Token::ControllerMethods
+
+	def connect
+    	self.current_user = find_verified_user
+    	reject_unauthorized_connection if self.current_user.nil?
+	end
+
+	protected
+
+	def find_verified_user
+    	authenticate_or_request_with_http_token do |token, options|
+      	User.find_by(auth_token: token)
+    end
   end
 end
--- app/channels/application_cable/room_channel.rb
+++ app/channels/application_cable/room_channel.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-  	puts 'ID: ' + params[:id]
   	stream_from 'locations'
     # stream_from "some_channel"
   end
