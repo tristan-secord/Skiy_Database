8d09e4b5280fdc5e6a481b560f7e9d07e81d08ad
tristan-secord <tristan.secord@hotmail.com>
2016-09-25 11:52:51 -0400

    Debugging/Test Commit

--- app/channels/application_cable/room_channel.rb
+++ app/channels/application_cable/room_channel.rb
@@ -1,6 +1,12 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
+  	if params
+  		puts params
+  	else
+  		puts 'NO params'
+  	end
+  	
   	stream_from 'RoomChannel#{current_user.id}'
     # stream_from "some_channel"
   end
@@ -11,6 +17,6 @@ class RoomChannel < ApplicationCable::Channel
 
   def locUpdate(data) 
   	#ADD TO SERVER (LOCATIONS)
-  	ActionCable.server.broadcast("locations", data)
+  	ActionCable.server.broadcast("RoomChannel#{current_user.id}", data)
   end
 end
